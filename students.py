3f7ca0e04911bf65f91333713c08c27eb94eb5b1
kamaumike <kamaumike2013@gmail.com>
2016-07-16 14:15:48 +0300

    created student dictionary

--- students.py
+++ students.py
@@ -1,1 +1,16 @@
-student = {}
+from datetime import datetime
+
+year = datetime.now().year
+
+student = {'name': ['Michael', 'Kamau'],
+'date_of_birth': 1990,
+'phone_number': '0725033033'
+}
+
+last_name = student.get('name')
+
+print last_name[1]
+print student.get('name')
+print student.get('phone_number')
+print student.get('date_of_birth')
+print year  - student.get('date_of_birth')
